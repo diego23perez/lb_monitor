1b71b67407dde7d4b6d1830d7042fce043c539ae
Diego Perez <pcascaron666@hotmail.com>
2021-06-12 18:49:55 -0300

    creacion de nuevo archivo

---
+++ logger_lb.py
@@ -0,0 +1,6 @@
+import logging
+logging.basicConfig(filename="lb_monitor.log", level=logging.INFO)
+logging.info('Este es un mensaje de info')
+logging.error('Este es un mnensaje de error')
+logging.debug('Este es un mensaje de debug')
+
