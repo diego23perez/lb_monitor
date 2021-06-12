bcb119742a1b20bf381d4b3fbcf59984179f5724
Diego Perez <pcascaron666@hotmail.com>
2021-06-12 19:22:38 -0300

    testing

--- logger_lb.py
+++ logger_lb.py
@@ -1,6 +1,6 @@
 import logging
 logging.basicConfig(filename="lb_monitor.log", level=logging.INFO)
-logging.info('Este es un mensaje de info')
-logging.error('Este es un mnensaje de error')
-logging.debug('Este es un mensaje de debug')
+logging.info('estos son otros mensajes de info')
+logging.error('Y ESTOS SON otros mensjaes de error')
+logging.debug('Este es unfffff mensaje de debug')
 
